34e298a83953daf25b42db56d9b83adf3e6c784a
Bilel <ng.bilel@gmail.com>
2019-07-19 18:49:12 +0200

    Full Stack App - Auth :  Creating Dashboard

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLayout from  '../..//Hoc/AdminLayout'
+
+const Dashboard = () => {
+    return (
+        <AdminLayout>
+        <div className='user_dashboard'>
+            this is your dashboard
+        </div>
+        </AdminLayout>
+    )
+};
+
+export default Dashboard
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import Layout from './Hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/home';
 import SingIn from './Components/signIn'
+import Dashboard from './Components/admin/Dashboard'
 
 const Routes = (props) => {
   return (
    
         <Layout>
           <Switch>
-            <Route exact component={SingIn} path="/Sing_in"/>
+          <Route exact component={Dashboard} path="/dashboard"/>
+            <Route exact component={SingIn} path="/sing_in"/>
             <Route exact component={Home} path="/"/>
           </Switch>
         </Layout>
