7d467d5cc1908e4f49b7286b10c5f8e70807cea3
Bilel <ng.bilel@gmail.com>
2019-07-19 16:33:16 +0200

    Full Stack App - Auth : Adding Login component

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/home';
+import SingIn from './Components/signIn'
 
 const Routes = (props) => {
   return (
    
         <Layout>
           <Switch>
+            <Route exact component={SingIn} path="/Sing_in"/>
             <Route exact component={Home} path="/"/>
           </Switch>
         </Layout>
