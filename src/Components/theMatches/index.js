b9498fdb02147ceec0a3cd30d62e1e41dfa26268
Bilel <ng.bilel@gmail.com>
2019-07-24 17:08:35 +0200

    Full Stack App - Matches : Adding Filters

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -11,7 +11,7 @@ class TheMatches extends Component {
         loading: true,
         matches:[],
         filterMatches:[],
-        playerFilter:'All',
+        playedFilter:'All',
         resultFilter:'All'
     }
 
@@ -27,15 +27,91 @@ class TheMatches extends Component {
         })
     }
 
+    showPlayed = (played) => {
+        const list = this.state.matches.filter((match)=>{
+            return match.final === played
+        });
+        this.setState({
+            filterMatches: played === "All" ? this.state.matches : list,
+            playedFilter: played,
+            resultFilter:'All'
+        })
+    }
+
+    showResult = (result) => {
+        const list = this.state.matches.filter((match)=>{
+            return match.result === result
+        });
+        this.setState({
+            filterMatches: result === "All" ? this.state.matches : list,
+            playerFilter: 'All',
+            resultFilter: result
+        })
+    }
+
     render(){
         const state=this.state;
         return (
             <div className="the_matches_container">
                 <div className="the_matches_wrapper">
                     <div className='left'>
-                        <MatchesList
-                            matches={state.filterMatches}
-                        />
+                        <div className="match_filters">
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                    Show Match
+                                </div>
+                                <div className="cont">
+                                    <div className={`option ${state.playedFilter === 'All' ? 'active' : ''}`} 
+                                         onClick={()=>this.showPlayed('All')} 
+                                    >
+                                        All 
+                                    </div>
+                                    <div className={`option ${state.playedFilter === 'Yes' ? 'active' : ''}`} 
+                                         onClick={()=>this.showPlayed('Yes')} 
+                                    >
+                                        Played 
+                                    </div>
+                                    <div className={`option ${state.playedFilter === 'No' ? 'active' : ''}`} 
+                                         onClick={()=>this.showPlayed('No')} 
+                                    >
+                                        Not Played 
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                     Result Game
+                                </div>
+
+                                <div className="cont">
+                                    <div className={`option ${state.resultFilter === 'All' ? 'active' : ''}`} 
+                                         onClick={()=>this.showResult('All')} 
+                                    >
+                                        All 
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'W' ? 'active' : ''}`} 
+                                         onClick={()=>this.showResult('W')} 
+                                    >
+                                        W 
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'L' ? 'active' : ''}`} 
+                                         onClick={()=>this.showResult('L')} 
+                                    >
+                                        L
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'D' ? 'active' : ''}`} 
+                                         onClick={()=>this.showResult('D')} 
+                                    >
+                                        D
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <MatchesList matches={state.filterMatches} />
+
                     </div>
                     <div className='right'>
                         <LeagueTable/>
--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -63,7 +63,27 @@ class MatchesList extends Component {
                                             transform:`translate(${x}px)`
                                         }}
                                     >
-                                        result
+                                        <div className="block_wrapper">
+                                            <div className="block">
+                                                <div className="icon" style={{background:`url(/images/team_icons/${data.localThmb}.png)`}}></div>
+                                                <div className="team">{data.local}</div>
+                                                <div className="result">{data.resultLocal}</div>
+
+                                            </div>
+                                            <div className="block">
+                                                <div className="icon" style={{background:`url(/images/team_icons/${data.awayThmb}.png)`}}></div>
+                                                <div className="team">{data.away}</div>
+                                                <div className="result">{data.resultAway}</div>
+
+                                            </div>
+                                        </div>
+    
+                                        <div className="block_wrapper info">
+                                            <div><strong>Date : </strong>{data.date}</div>
+                                            <div><strong>Stadium : </strong>{data.stadium}</div>
+                                            <div><strong>Referee : </strong>{data.referee}</div>
+                                        </div>
+
                                     </div>
                                 ))
                             }
